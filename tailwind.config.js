bc42b47e7a81b5ddce989dc0b4343e2e3b1bceb5
yelaD <dale@internold.com>
2020-08-06 09:51:31 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      'karla': ['Karla', 'sans-serif']
+    },
+
+    extend: {
+      colors: {
+        primary: {
+          cyan: "#2AB2AF",
+          yellow: "#C0DF34",
+          lightBlue: "#4ABEBD"
+        },
+        neutral: {
+          lightGray: "#E5EFF5",
+          grayishBlue: "#98A6BD"
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
